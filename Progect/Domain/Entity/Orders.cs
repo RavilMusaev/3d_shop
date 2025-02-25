0a6684b2e7f50d1a750ec068497a2cfc51b2280e
RavilMusaev <musaev_r@iuca.kg>
2025-02-25 16:09:43 +0600

    3d_shop

---
+++ Progect/Domain/Entity/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public int Id_user { get; set; }
+        public int Final_sum { get; set; }
+        public string Status_orders { get; set; }
+        public string Date_order { get; set; }
+        public string Date_end { get; set; }
+        public string comment { get; set; }
+    }
+}
